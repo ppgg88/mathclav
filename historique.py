74d7c954484d18698f39631692babddcced6ef15
ppgg88 <32483270+ppgg88@users.noreply.github.com>
2022-07-17 23:31:18 +0200

    affichage historique

--- historique.py
+++ historique.py
@@ -10,6 +10,8 @@ import sys
 import os
 import pickle
 
+matplotlib.use('TkAgg')
+
 class historique(tk.Frame):
     def __init__(self, result, parent):
         self.parent = parent
@@ -30,8 +32,26 @@ class historique(tk.Frame):
         master.title("MathClav_Historique")
         tk.Frame.__init__(self, master)
 
-        self.label_actuel = tk.Label(self, text=result.str())
-        self.label_actuel.grid(row=0, column=0)
+        tmptext = self.result.str()
+        tmptext = tmptext.replace(r"\newline", "$ \n $")
+        tmptext = tmptext.replace(r"æ", "a")
+
+        self.fig_=[]
+        self.canvas_=[]
+        self.wx_=[]
+
+        self.fig = plt.Figure(figsize=(4, 0.5), dpi=100)
+        self.wx = self.fig.add_subplot(111)
+        self.wx.get_xaxis().set_visible(False)
+        self.wx.get_yaxis().set_visible(False)
+        self.wx.patch.set_visible(False)
+        self.wx.axis('off')
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.get_tk_widget()
+        self.wx.text(-0.1, 0.6, r"$"+tmptext.replace(r"\text",r"\mathrm")+"$", fontsize =   8)
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+        self.canvas.draw()
+
 
         self.nom_actuel = tk.Entry(self)
         self.nom_actuel.grid(row=0, column=1)
@@ -56,9 +76,23 @@ class historique(tk.Frame):
                 self.result_.append(pickle.load(f1))
             self.label_historique_name.append(tk.Label(self, text=self.result_[i-1].name))
             self.label_historique_name[len(self.label_historique_name)-1].grid(row=i, column=0)
-            self.label_historique.append(tk.Label(self, text=self.result_[i-1].str()))
-            self.label_historique[len(self.label_historique)-1].grid(row=i, column=1)
-            self.label_historique[len(self.label_historique)-1].bind("<Button-1>", lambda event, i=i: self.parent.ajout(self.result_[i-1]))
+            
+            tmptext = self.result_[len(self.result_)-1].str()
+            tmptext = tmptext.replace(r"\newline", "$ \n $")
+            tmptext = tmptext.replace(r"æ", "a")
+            self.fig_.append(plt.Figure(figsize=(4, 0.5), dpi=100))
+            self.wx = self.fig_[len(self.fig_)-1].add_subplot(111)
+            self.wx.get_xaxis().set_visible(False)
+            self.wx.get_yaxis().set_visible(False)
+            self.wx.patch.set_visible(False)
+            self.wx.axis('off')
+            self.canvas_.append(FigureCanvasTkAgg( self.fig_[len(self.fig_)-1], master=self))
+            self.canvas_[len(self.canvas_)-1].get_tk_widget()
+            self.wx.text(-0.1, 0.6, r"$"+tmptext.replace(r"\text",r"\mathrm")+"$", fontsize =   8)
+            self.canvas_[len(self.canvas_)-1].get_tk_widget().grid(row=i, column=1)
+            self.canvas_[len(self.canvas_)-1].draw()
+
+            #self.canvas[len(self.fig_)-1].bind("<Button-1>", lambda event, i=i: self.parent.ajout(self.result_[i-1]))
             self.button_supr.append(tk.Button(self, text="Suprimer", command=(lambda i=i:self.supr(i-1))))
             self.button_supr[len(self.button_supr)-1].grid(row=i, column=2)
 
@@ -73,20 +107,11 @@ class historique(tk.Frame):
     
     def supr(self, i):
         os.remove(r'historique\hist_'+str(self.nb_historique)+'.pkl')
-        self.result_.pop(i)
-        self.label_historique[i].destroy()
-        self.button_supr[i].destroy()
-        self.label_historique_name[i].destroy()
-        self.button_supr.pop(i)
-        self.label_historique.pop(i)
-        self.label_historique_name.pop(i)
         self.nb_historique -= 1
         with open(r'historique\nb.pkl', 'wb') as f1:
             pickle.dump(self.nb_historique, f1)
-        for i in range(1, len(self.result_)):
-            with open(r'historique\hist_'+str(i)+'.pkl', 'wb') as f1:
-                pickle.dump(self.result_[i], f1)
-        self.afficher()
+        h = historique(self.result, self.parent)
+        self.master.destroy()
 
 if __name__ == '__main__':
     h = historique(mathObject())
